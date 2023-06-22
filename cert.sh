be979fdc19453df9dfdb004528ef117b1e340bc1
Leonid Kartsev <karcev.l@mail.ru>
2023-06-22 17:21:54 +0300

    21

--- cert.sh
+++ cert.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-  domains=(www.rossulbricht.fun rossulbricht.fun)
+  domains=(rossulbricht.fun)
   rsa_key_size=4096
   data_path="./certbot"
   email="karcev.l@mail.ru"
