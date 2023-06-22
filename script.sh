512c37c9ab38433615aa2f6e69032b28ae3d63ba
Leonid Kartsev <karcev.l@mail.ru>
2023-06-22 11:17:18 +0300

    16

--- script.sh
+++ script.sh
@@ -1,13 +1,19 @@
 sudo apt update
 sudo apt upgrade -y
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
-sudo apt update
-sudo apt install docker-ce -y
-sudo gpasswd -a ubuntu docker
-
-sudo apt install docker-compose
-
-cd spring-petclinic   
-sudo docker-compose up --build
+git clone https://github.com/RossUlbricht1/spring-petclinic.git
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \"deb [arch=\\\"$(dpkg --print-architecture)\\\" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \\\"$(. /etc/os-release && echo \\\"$VERSION_CODENAME\\\")\\\")\\\" stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo usermod -aG docker $USER
+sudo apt install docker-compose -y
+echo '${yandex_compute_instance.vm-1.network_interface.0.nat_ip_address}' > /home/ubuntu/spring-petclinic/ip-address.txt
+cd ./spring-petclinic
+sudo docker compose -f docker-compose.yml -f docker-compose_nginx.yml up --build -d
+sudo iptables -A INPUT -p tcp -m multiport --dports 22,80,443 -j ACCEPT
+sudo iptables -A INPUT -j DROP
+sudo iptables-save | sudo tee /etc/iptables/rules.v4
+sudo iptables -L
