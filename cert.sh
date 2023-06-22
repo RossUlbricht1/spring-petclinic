e8fb5aa9d13e92ab50b6bf5831dfa37f45d553ee
Ubuntu <ubuntu@fhmimmnd7jvb9s33kpkf.auto.internal>
2023-06-22 13:10:57 +0000

    18

--- cert.sh
+++ cert.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-exec 2>> script_logs.txt
-
 if ! [ -x "$(command -v docker-compose)" ]; then
   echo 'Error: docker compose is not installed.' >&2
   exit 1
@@ -11,7 +9,7 @@ fi
   rsa_key_size=4096
   data_path="./certbot"
   email="karcev.l@mail.ru"
-  staging=1
+  staging="1"
 
 if [ -d "$data_path" ]; then
     read -p "Existing data found for $domains Continue and replace existing certificate? (y/N)" decision
@@ -32,14 +30,14 @@ echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
 docker-compose -f docker-compose_nginx.yml run --rm --entrypoint "\
-  openssl req -x509 -nodes -newkey rsa:1024 -days 1\
+  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
   -keyout '$path/privkey.pem'\
   -out '$path/fullchain.pem'\
   -subj '/CN=localhost'" certbot
 echo
 
 echo "### Starting nginx ..."
-docker-compose -f docker-compose_nginx.yml up --force-recreate -d nginxserver
+docker-compose -f docker-compose_nginx.yml up --force-recreate -d nginx
 echo
 
 echo "### Deleting dummy certificate for $domains"
@@ -74,4 +72,4 @@ docker-compose -f docker-compose_nginx.yml run --rm --entrypoint "\
 echo
 
 echo "### Reloading nginx ..."
-docker-compose exec nginxserver nginx -s reload
+docker exec nginx nginx -s reload
